3587af5e9549c12573b81b58b4d87b0dc1c549a2
pneumaier1 <55095827+pneumaier1@users.noreply.github.com>
2019-11-04 12:03:26 -0500

    Version 1.2
    Adding main to allow the game to run outside of java

--- Game.java
+++ Game.java
@@ -40,6 +40,12 @@ public class Game
         parser = new Parser();
     }
 
+    public static void main(String[] args){
+        Game myGame = new Game();
+        myGame.play();
+    }
+    
+    
     /**
      * Create all the rooms and link their exits together.
      */
